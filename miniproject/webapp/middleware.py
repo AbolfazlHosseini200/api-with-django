91f0b7cda3cc2c5389e854964db17d42bc973336
abolfazlhosseini200 <abolfazlsobhan@gmail.com>
2022-01-01 22:46:31 +0330

    adding test

--- miniproject/webapp/middleware.py
+++ miniproject/webapp/middleware.py
@@ -1,7 +1,5 @@
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
-from webapp.models import User
 from django.utils.crypto import get_random_string
+
 
 def create_token(users):
     token = get_random_string(length=50)
--- miniproject/webapp/tests.py
+++ miniproject/webapp/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from django.test.client import Client
+from rest_framework.utils import json
 
-# Create your tests here.
+
+class UrlTest(TestCase):
+    def test_register(self):
+        jsons = {
+            "first_name": "abolfazl",
+            "last_name": "hosseini",
+            "number": "fake@email",
+            "password": "1234",
+            "password2": "1234",
+            "username": "abolfazl81"
+        }
+        response = self.client.post('/register/', json.dumps(jsons), 'json',
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest', )
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        jsons = {
+            "token": "",
+        }
+        response = self.client.post('/logout/', json.dumps(jsons), 'json',
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest', )
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        jsons = {
+            "username": "abolfazl81",
+            "password": "1234"
+        }
+        response = self.client.post('/login/', json.dumps(jsons), 'json',
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest', )
+        self.assertEqual(response.status_code, 200)
+
+    def test_insurance(self):
+        jsons = {
+            "age": "20",
+            "email": "1234",
+            "smoke": 'False',
+            "bmi": "25.0",
+            "token": ""
+        }
+        response = self.client.post('/insurance/', json.dumps(jsons), 'json',
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest', )
+        self.assertEqual(response.status_code, 200)
